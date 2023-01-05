3091f71d4c37a1c20029be7b639cd5c04b19e625
NouriMohammed <nourimohammed93@gmail.com>
2023-01-05 11:36:01 +0100

    update the project

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -15,6 +15,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Utils\Validator;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
@@ -22,7 +23,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
 use function Symfony\Component\String\u;
 
@@ -46,30 +47,21 @@ use function Symfony\Component\String\u;
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  * @author Yonel Ceruto <yonelceruto@gmail.com>
  */
+#[AsCommand(
+    name: 'app:add-user',
+    description: 'Creates users and stores them in the database'
+)]
 class AddUserCommand extends Command
 {
-    // to make your command lazily loaded, configure the $defaultName static property,
-    // so it will be instantiated only when the command is actually called.
-    protected static $defaultName = 'app:add-user';
+    private SymfonyStyle $io;
 
-    /**
-     * @var SymfonyStyle
-     */
-    private $io;
-
-    private $entityManager;
-    private $passwordEncoder;
-    private $validator;
-    private $users;
-
-    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, Validator $validator, UserRepository $users)
-    {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+        private Validator $validator,
+        private UserRepository $users
+    ) {
         parent::__construct();
-
-        $this->entityManager = $em;
-        $this->passwordEncoder = $encoder;
-        $this->validator = $validator;
-        $this->users = $users;
     }
 
     /**
@@ -78,7 +70,6 @@ class AddUserCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Creates users and stores them in the database')
             ->setHelp($this->getCommandHelp())
             // commands can optionally define arguments and/or options (mandatory and optional)
             // see https://symfony.com/doc/current/components/console/console_arguments.html
@@ -112,7 +103,7 @@ class AddUserCommand extends Command
      * quite a lot of work. However, if the command is meant to be used by external
      * users, this method is a nice way to fall back and prevent errors.
      */
-    protected function interact(InputInterface $input, OutputInterface $output)
+    protected function interact(InputInterface $input, OutputInterface $output): void
     {
         if (null !== $input->getArgument('username') && null !== $input->getArgument('password') && null !== $input->getArgument('email') && null !== $input->getArgument('full-name')) {
             return;
@@ -138,7 +129,9 @@ class AddUserCommand extends Command
         }
 
         // Ask for the password if it's not defined
+        /** @var string|null $password */
         $password = $input->getArgument('password');
+
         if (null !== $password) {
             $this->io->text(' > <info>Password</info>: '.u('*')->repeat(u($password)->length()));
         } else {
@@ -174,25 +167,33 @@ class AddUserCommand extends Command
         $stopwatch = new Stopwatch();
         $stopwatch->start('add-user-command');
 
+        /** @var string $username */
         $username = $input->getArgument('username');
+
+        /** @var string $plainPassword */
         $plainPassword = $input->getArgument('password');
+
+        /** @var string $email */
         $email = $input->getArgument('email');
+
+        /** @var string $fullName */
         $fullName = $input->getArgument('full-name');
+
         $isAdmin = $input->getOption('admin');
 
         // make sure to validate the user data is correct
         $this->validateUserData($username, $plainPassword, $email, $fullName);
 
-        // create the user and encode its password
+        // create the user and hash its password
         $user = new User();
         $user->setFullName($fullName);
         $user->setUsername($username);
         $user->setEmail($email);
         $user->setRoles([$isAdmin ? 'ROLE_ADMIN' : 'ROLE_USER']);
 
-        // See https://symfony.com/doc/current/security.html#c-encoding-passwords
-        $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
-        $user->setPassword($encodedPassword);
+        // See https://symfony.com/doc/5.4/security.html#registering-the-user-hashing-passwords
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
@@ -207,7 +208,7 @@ class AddUserCommand extends Command
         return Command::SUCCESS;
     }
 
-    private function validateUserData($username, $plainPassword, $email, $fullName): void
+    private function validateUserData(string $username, string $plainPassword, string $email, string $fullName): void
     {
         // first check if a user with the same username already exists.
         $existingUser = $this->users->findOneBy(['username' => $username]);
@@ -237,27 +238,26 @@ class AddUserCommand extends Command
     private function getCommandHelp(): string
     {
         return <<<'HELP'
-The <info>%command.name%</info> command creates new users and saves them in the database:
+            The <info>%command.name%</info> command creates new users and saves them in the database:
 
-  <info>php %command.full_name%</info> <comment>username password email</comment>
+              <info>php %command.full_name%</info> <comment>username password email</comment>
 
-By default the command creates regular users. To create administrator users,
-add the <comment>--admin</comment> option:
+            By default the command creates regular users. To create administrator users,
+            add the <comment>--admin</comment> option:
 
-  <info>php %command.full_name%</info> username password email <comment>--admin</comment>
+              <info>php %command.full_name%</info> username password email <comment>--admin</comment>
 
-If you omit any of the three required arguments, the command will ask you to
-provide the missing values:
+            If you omit any of the three required arguments, the command will ask you to
+            provide the missing values:
 
-  # command will ask you for the email
-  <info>php %command.full_name%</info> <comment>username password</comment>
+              # command will ask you for the email
+              <info>php %command.full_name%</info> <comment>username password</comment>
 
-  # command will ask you for the email and password
-  <info>php %command.full_name%</info> <comment>username</comment>
+              # command will ask you for the email and password
+              <info>php %command.full_name%</info> <comment>username</comment>
 
-  # command will ask you for all arguments
-  <info>php %command.full_name%</info>
-
-HELP;
+              # command will ask you for all arguments
+              <info>php %command.full_name%</info>
+            HELP;
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,16 +11,16 @@
 
 namespace App\Entity;
 
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\Table(name="symfony_demo_user")
- *
  * Defines the properties of the User entity to represent the application users.
- * See https://symfony.com/doc/current/doctrine.html#creating-an-entity-class
+ * See https://symfony.com/doc/current/doctrine.html#creating-an-entity-class.
  *
  * Tip: if you have an existing database, you can generate these entity class automatically.
  * See https://symfony.com/doc/current/doctrine/reverse_engineering.html
@@ -28,55 +28,36 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @author Ryan Weaver <weaverryan@gmail.com>
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
-class User implements UserInterface, \Serializable
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'symfony_demo_user')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @var int
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING)]
+    #[Assert\NotBlank]
+    private ?string $fullName = null;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    #[Assert\Email]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::STRING)]
+    private ?string $password = null;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(type="string")
-     * @Assert\NotBlank()
+     * @var string[]
      */
-    private $fullName;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", unique=true)
-     * @Assert\NotBlank()
-     * @Assert\Length(min=2, max=50)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", unique=true)
-     * @Assert\Email()
-     */
-    private $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string")
-     */
-    private $password;
-
-    /**
-     * @var array
-     *
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
+    #[ORM\Column(type: Types::JSON)]
+    private array $roles = [];
 
     public function getId(): ?int
     {
@@ -93,9 +74,14 @@ class User implements UserInterface, \Serializable
         return $this->fullName;
     }
 
-    public function getUsername(): ?string
+    public function getUserIdentifier(): string
     {
-        return $this->username;
+        return (string) $this->username;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->getUserIdentifier();
     }
 
     public function setUsername(string $username): void
@@ -138,6 +124,9 @@ class User implements UserInterface, \Serializable
         return array_unique($roles);
     }
 
+    /**
+     * @param string[] $roles
+     */
     public function setRoles(array $roles): void
     {
         $this->roles = $roles;
@@ -151,7 +140,7 @@ class User implements UserInterface, \Serializable
     public function getSalt(): ?string
     {
         // We're using bcrypt in security.yaml to encode the password, so
-        // the salt value is built-in and and you don't have to generate one
+        // the salt value is built-in and you don't have to generate one
         // See https://en.wikipedia.org/wiki/Bcrypt
 
         return null;
@@ -169,20 +158,20 @@ class User implements UserInterface, \Serializable
     }
 
     /**
-     * {@inheritdoc}
+     * @return array{int|null, string|null, string|null}
      */
-    public function serialize(): string
+    public function __serialize(): array
     {
         // add $this->salt too if you don't use Bcrypt or Argon2i
-        return serialize([$this->id, $this->username, $this->password]);
+        return [$this->id, $this->username, $this->password];
     }
 
     /**
-     * {@inheritdoc}
+     * @param array{int|null, string, string} $data
      */
-    public function unserialize($serialized): void
+    public function __unserialize(array $data): void
     {
         // add $this->salt too if you don't use Bcrypt or Argon2i
-        [$this->id, $this->username, $this->password] = unserialize($serialized, ['allowed_classes' => false]);
+        [$this->id, $this->username, $this->password] = $data;
     }
 }
